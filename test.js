7a43f187942ec41c39f82b36cd25a4ac22db80fc
Danny Robinson <daniel@djrobinson.me>
2016-05-03 16:27:23 -0600

    Reimplemented redux store

--- test.js
+++ test.js
@@ -9,7 +9,39 @@ const counter = (state = 0, action) => {
   }
 };
 
-const { createStore } = Redux;
+//Reimplementing the Redux Store
+
+//createStore takes a reducer that will modify state
+const createStore = (reducer) => {
+
+  //creating state variable, list of listeners
+  let state;
+  let listeners = [];
+
+  //get state returns the current state
+  const getState = () => state;
+
+  //dispatch is the only way to change the current state via the reducer
+  //it will notify each listener after being called
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  //Subscribe will add listener to listener array.
+  //returns function that will remove unsubscribed components
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};
+
 const store = createStore(counter);
 
 const render = () => {
